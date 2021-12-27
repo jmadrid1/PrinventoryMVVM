50ad7d44ce3da1eee6303541cdd387138033c62c
jmadrid1 <31678109+jmadrid1@users.noreply.github.com>
2021-12-26 17:24:59 -0800

    Removed the use of Synthetics and refactored code
    Removed the use of Synthetics and refactored code

--- app/src/main/java/com/example/prinventory_mvvm/ui/printer/PrinterFragment.kt
+++ app/src/main/java/com/example/prinventory_mvvm/ui/printer/PrinterFragment.kt
@@ -1,18 +1,19 @@
 package com.example.prinventory_mvvm.ui.printer
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.prinventory_mvvm.R
 import com.example.prinventory_mvvm.adapters.PrinterAdapter
+import com.example.prinventory_mvvm.databinding.FragmentPrinterBinding
 import com.example.prinventory_mvvm.models.Printer
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_printer.*
 
 @AndroidEntryPoint
 class PrinterFragment : Fragment(R.layout.fragment_printer) {
@@ -21,8 +22,20 @@ class PrinterFragment : Fragment(R.layout.fragment_printer) {
         const val BUNDLE_KEY = "printer"
     }
 
+    private var _binding: FragmentPrinterBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel : PrinterViewModel by viewModels()
     private lateinit var printerAdapter : PrinterAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPrinterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -40,11 +53,12 @@ class PrinterFragment : Fragment(R.layout.fragment_printer) {
             hideEmptyInventoryPrompts(it)
             printerAdapter.differ.submitList(it)
         }
+
     }
 
     private fun setupRecyclerView(){
         printerAdapter = PrinterAdapter()
-        fragment_printer_recyclerview.apply{
+        binding.fragmentPrinterRecyclerview.apply{
             adapter = printerAdapter
             layoutManager = LinearLayoutManager(context)
         }
@@ -52,13 +66,13 @@ class PrinterFragment : Fragment(R.layout.fragment_printer) {
 
     private fun hideEmptyInventoryPrompts(printers: List<Printer>){
         if(printers.isEmpty()){
-            fragment_printer_textview_empty_msg_1.visibility = View.VISIBLE
-            fragment_printer_textview_empty_msg_2.visibility = View.VISIBLE
-            fragment_printer_recyclerview.visibility = View.GONE
+            binding.fragmentPrinterTextviewEmptyMsg1.visibility = View.VISIBLE
+            binding.fragmentPrinterTextviewEmptyMsg2.visibility = View.VISIBLE
+            binding.fragmentPrinterRecyclerview.visibility = View.GONE
         }else {
-            fragment_printer_textview_empty_msg_1.visibility = View.GONE
-            fragment_printer_textview_empty_msg_2.visibility = View.GONE
-            fragment_printer_recyclerview.visibility = View.VISIBLE
+            binding.fragmentPrinterTextviewEmptyMsg1.visibility = View.GONE
+            binding.fragmentPrinterTextviewEmptyMsg2.visibility = View.GONE
+            binding.fragmentPrinterRecyclerview.visibility = View.VISIBLE
         }
     }
 
