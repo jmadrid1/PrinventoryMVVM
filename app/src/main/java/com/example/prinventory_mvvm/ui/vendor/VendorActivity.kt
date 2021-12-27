381b28aeed5b3a494213f6587cc90b67141c9c5e
jmadrid1 <31678109+jmadrid1@users.noreply.github.com>
2021-12-26 17:25:34 -0800

    Removed the use of Synthetics and refactored code
    Removed the use of Synthetics and refactored code

--- app/src/main/java/com/example/prinventory_mvvm/ui/vendor/VendorActivity.kt
+++ app/src/main/java/com/example/prinventory_mvvm/ui/vendor/VendorActivity.kt
@@ -5,81 +5,76 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.findNavController
 import com.example.prinventory_mvvm.R
+import com.example.prinventory_mvvm.databinding.ActivityVendorBinding
 import com.example.prinventory_mvvm.ui.printer.PrinterActivity
 import com.example.prinventory_mvvm.ui.toner.TonerActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_printer.*
-import kotlinx.android.synthetic.main.activity_toner.*
-import kotlinx.android.synthetic.main.activity_toolbar.*
-import kotlinx.android.synthetic.main.activity_vendor.*
-import kotlinx.android.synthetic.main.activity_vendor.activity_toolbar
-import kotlinx.android.synthetic.main.activity_vendor.navHostFragment
 
 @AndroidEntryPoint
 class VendorActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
-    
+
+    private lateinit var binding: ActivityVendorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vendor)
+        binding = ActivityVendorBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        activity_toolbar_add.setOnClickListener {
-            navHostFragment.findNavController().navigate(R.id.vendorCreateFragment)
+        val navController = findNavController(R.id.navHostFragment)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.vendorFragment -> { showToolbars() }
+                R.id.vendorCreateFragment -> { hideToolbars() }
+                R.id.vendorDetailFragment -> { hideToolbars() }
+                R.id.vendorEditFragment -> { hideToolbars() }
+            }
         }
 
-        vendor_bottomnavigation.setupWithNavController(navHostFragment.findNavController())
+        binding.activityToolbar.activityToolbarAdd.setOnClickListener {
+            findNavController(R.id.navHostFragment).navigate(R.id.vendorCreateFragment)
+        }
 
-        navHostFragment.findNavController()
-                .addOnDestinationChangedListener { _, destination, _ ->
-                    when (destination.id) {
-                        R.id.vendorFragment -> {
-                            activity_toolbar.visibility = View.VISIBLE
-                            vendor_bottomnavigation.visibility = View.VISIBLE
-                        }
-                        R.id.vendorCreateFragment -> {
-                            activity_toolbar.visibility = View.GONE
-                            vendor_bottomnavigation.visibility = View.GONE
-                        }
-                        R.id.vendorDetailFragment -> {
-                            activity_toolbar.visibility = View.GONE
-                            vendor_bottomnavigation.visibility = View.GONE
-                        }
-                        R.id.vendorEditFragment -> {
-                            activity_toolbar.visibility = View.GONE
-                            vendor_bottomnavigation.visibility = View.GONE
-                        }
-                    }
-                }
+        binding.vendorBottomnavigation.menu.findItem(R.id.navigation_menu_vendor).isChecked = true
+        binding.vendorBottomnavigation!!.setOnNavigationItemSelectedListener(this)
+    }
 
-        vendor_bottomnavigation.menu.findItem(R.id.navigation_menu_vendor).isChecked = true
-        vendor_bottomnavigation!!.setOnNavigationItemSelectedListener(this)
+    private fun hideToolbars(){
+        binding.activityToolbar.root.visibility = View.GONE
+        binding.vendorBottomnavigation.visibility = View.GONE
+    }
+
+    private fun showToolbars(){
+        binding.activityToolbar.root.visibility = View.VISIBLE
+        binding.vendorBottomnavigation.visibility = View.VISIBLE
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.navigation_menu_printer -> {
-                val intent = Intent(this@VendorActivity, PrinterActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                startActivity(intent)
-                overridePendingTransition(0, 0)
-            }
-
-
-            R.id.navigation_menu_toner -> {
-                val intent = Intent(this@VendorActivity, TonerActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                startActivity(intent)
-                overridePendingTransition(0, 0)
-            }
-
-            R.id.navigation_menu_vendor -> {   }
+            R.id.navigation_menu_printer -> { navigateToPrinters() }
+            R.id.navigation_menu_toner -> { navigateToToners() }
+            R.id.navigation_menu_vendor -> { }
         }
         return true
+    }
+
+    private fun navigateToPrinters(){
+        val intent = Intent(this@VendorActivity, PrinterActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        overridePendingTransition(0, 0)
+    }
+
+    private fun navigateToToners(){
+        val intent = Intent(this@VendorActivity, TonerActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        overridePendingTransition(0, 0)
     }
 
 }
