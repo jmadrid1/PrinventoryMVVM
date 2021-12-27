e9b2adb18ad98aed8c6db5763d09268afb430ace
jmadrid1 <31678109+jmadrid1@users.noreply.github.com>
2021-12-26 17:21:56 -0800

    Removed the use of Synthetics
    Removed the use of Synthetics

--- app/src/main/java/com/example/prinventory_mvvm/adapters/VendorAdapter.kt
+++ app/src/main/java/com/example/prinventory_mvvm/adapters/VendorAdapter.kt
@@ -1,18 +1,16 @@
 package com.example.prinventory_mvvm.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.prinventory_mvvm.R
+import com.example.prinventory_mvvm.databinding.ListRowVendorBinding
 import com.example.prinventory_mvvm.models.Vendor
-import kotlinx.android.synthetic.main.list_row_vendor.view.*
 
 class VendorAdapter : RecyclerView.Adapter<VendorAdapter.VendorViewHolder>() {
 
-    inner class VendorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class VendorViewHolder(val binding: ListRowVendorBinding) : RecyclerView.ViewHolder(binding.root)
 
     private val diffCallback = object : DiffUtil.ItemCallback<Vendor>() {
         override fun areItemsTheSame(oldItem: Vendor, newItem: Vendor): Boolean {
@@ -27,15 +25,16 @@ class VendorAdapter : RecyclerView.Adapter<VendorAdapter.VendorViewHolder>() {
     val differ = AsyncListDiffer(this, diffCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VendorViewHolder {
-        return VendorViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_row_vendor, parent, false))
+        val binding = ListRowVendorBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VendorViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: VendorViewHolder, position: Int) {
         val vendor = differ.currentList[position]
 
-        holder.itemView.row_vendor_name.text = vendor.name
-        holder.itemView.row_vendor_phone.text = "Phone: " + vendor.phone
-        holder.itemView.row_vendor_email.text = "Email: " + vendor.email
+        holder.binding.rowVendorName.text = vendor.name
+        holder.binding.rowVendorPhone.text = "Phone: " + vendor.phone
+        holder.binding.rowVendorEmail.text = "Email: " + vendor.email
 
         holder.itemView.setOnClickListener {
             onItemClickListener?.let {
